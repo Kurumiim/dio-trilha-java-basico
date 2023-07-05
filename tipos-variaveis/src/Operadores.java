5741daf19d2082c91827f445a8bb719e81c48a36
Rukattsu <rukattsu@gmail.com>
2023-07-05 16:28:44 -0300

    Operador unário incremento e decremento

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,5 +1,6 @@
 public class Operadores {
     public static void main(String[] args) {
+        /*
         String  nomeCompleto = "LINGUAGEM" + "JAVA";
         System.out.println(nomeCompleto);
 
@@ -16,6 +17,18 @@ public class Operadores {
         concatenacao = "1"+1+1+1;
         System.out.println(concatenacao);
         concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        System.out.println(concatenacao); */
+
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+
+
     }
 }

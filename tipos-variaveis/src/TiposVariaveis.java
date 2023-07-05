3ba032b204c295ba254f78f68b6fa4c2d79f7c31
Rukattsu <rukattsu@gmail.com>
2023-07-05 11:44:10 -0300

    complementando o estudo dos tipos-variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        
+        // Tipos primitivos
+
+        //Estudar a classe String que representa textos na aplicação.
+
+        String meuNome = "Lucas Fagundes";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
